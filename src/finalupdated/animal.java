8ed624f08e8fca1172585875ec369875f8d2a1c4
ADMIN <ADMIN@DESKTOP-RORU9LI>
2021-08-17 11:34:33 +0530

    login

--- src/finalupdated/animal.java
+++ src/finalupdated/animal.java
@@ -5,6 +5,7 @@ public class animal {
 	public static void main(String[] args) {
 		System.out.println("Hii tiger here");
 		System.out.println("Hii lion here");
+		System.out.println("login added");
 
 	}
 
